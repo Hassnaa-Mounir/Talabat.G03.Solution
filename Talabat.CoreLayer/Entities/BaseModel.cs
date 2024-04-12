82a6e0c6ce548440e37f2ddac04e9c5f931592d6
pc <pc@DESKTOP-2MO1OGA>
2024-04-12 06:39:50 +0200

    Core layer - product Module - Entities

---
+++ Talabat.CoreLayer/Entities/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.CoreLayer.Entities
+{
+    //Have Common Code between Entities
+    internal class BaseModel
+    {
+        public int Id { get; set; }
+    }
+}
