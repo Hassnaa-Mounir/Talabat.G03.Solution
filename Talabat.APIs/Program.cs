30db6c4bcf25de3f88104f0e1ced9287b0eba037
pc <pc@DESKTOP-2MO1OGA>
2024-04-12 08:37:41 +0200

    Update Database from program class

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -5,7 +5,7 @@ namespace Talabat.APIs
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +24,21 @@ namespace Talabat.APIs
             #endregion
 
             var app = builder.Build();
+
+            #region Update-DataBase
+            //StoreContext dbcontext = new StoreContext();// invalid
+            //await dbcontext.Database.MigrateAsync();
+
+            /// Ask CLR ToCreate object from dbcontext Explicitly
+           using var Scope = app.Services.CreateScope();
+            // Group of Services LifeTime Scoped
+            var services =Scope.ServiceProvider;
+            // you will  have service itself
+            var dbcontext =services.GetRequiredService<StoreContext>(); 
+            // you will have object from dbcontextClass by CLR Explicitly
+            await dbcontext.Database.MigrateAsync();
+             //update DataBase
+            #endregion
 
             // Configure the HTTP request pipeline.
 
