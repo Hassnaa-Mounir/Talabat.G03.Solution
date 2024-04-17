c8267b0193b235049d1345610c3302c371201d7e
pc <pc@DESKTOP-2MO1OGA>
2024-04-17 04:22:29 +0200

    Implement Specification Design Pattern

---
+++ Talabat.CoreLayer/Repositories/SpecificationsEvaluator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.CoreLayer.Entities;
+using Talabat.CoreLayer.Specifications;
+
+namespace Talabat.CoreLayer.Repositories
+{
+    internal static class SpecificationsEvaluator<TEntity> where TEntity : BaseModel
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+
+            if (spec.Criteria is not null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            query = spec.Includes.Aggregate(query, (currentQuery, includeExpressiom) => currentQuery.Include(includeExpressiom));
+
+            return query;
+        }
+    }
+}
--- Talabat.RepositoryLayer/GenericRepository.cs
+++ Talabat.RepositoryLayer/GenericRepository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Talabat.CoreLayer.Entities;
 using Talabat.CoreLayer.Repositories;
+using Talabat.CoreLayer.Specifications;
 using Talabat.RepositoryLayer.Data;
 
 namespace Talabat.RepositoryLayer
@@ -30,10 +31,19 @@ namespace Talabat.RepositoryLayer
             return await dbcontext.Set<T>().ToListAsync();   
         }
 
-        public async Task<T> GetByIdAsync(int id)
+        public async Task<IEnumerable<T>> GetAllWithSpecAsync(ISpecification<T> spec)
         {
-            //return await dbcontext.Set<T>().Where(x => x.Id == id).FirstOrDefaultAsync();
-            return await dbcontext.Set<T>().FindAsync(id); // search localy first
+            return await ApplySpecifications(spec).ToListAsync();
+        }
+
+        public async Task<T?> GetWithSpecAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecifications(spec).FirstOrDefaultAsync();
+        }
+
+        private IQueryable<T> ApplySpecifications(ISpecification<T> spec)
+        {
+            return SpecificationsEvaluator<T>.GetQuery(_dbContext.Set<T>(), spec);
         }
     }
 }
