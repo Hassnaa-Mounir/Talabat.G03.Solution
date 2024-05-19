fa6004456118ef9d59caaa4f84981961c12cd340
pc <pc@DESKTOP-2MO1OGA>
2024-05-19 22:05:19 +0300

    OrderItemPictureUrlResolver

--- Talabat.APIs/Helpers/MappingProfile.cs
+++ Talabat.APIs/Helpers/MappingProfile.cs
@@ -31,7 +31,8 @@ namespace Talabat.APIs.Helpers
             CreateMap<OrderAddress.OrderItem, OrderItemDto>()
                 .ForMember(d => d.ProductId, o => o.MapFrom(s => s.Product.ProductId))
                 .ForMember(d => d.ProductName, o => o.MapFrom(s => s.Product.ProductName))
-                .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.Product.PictureUrl));
+                .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.Product.PictureUrl))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<OrderItemPictureUrlResolver>());
 
         }
     }
