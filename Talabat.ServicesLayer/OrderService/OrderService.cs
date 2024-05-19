20a8ce007875e9ed27a811a87a821413cc44ba83
pc <pc@DESKTOP-2MO1OGA>
2024-05-19 12:50:19 +0300

    order service

---
+++ Talabat.ServicesLayer/OrderService/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.CoreLayer.Entities.Order_Aggregate;
+using Talabat.CoreLayer.Services;
+
+namespace Talabat.ServicesLayer.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string buyerEmail, string basketId, string deliveryMethodId, Address shippingAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdForUserAsync(string buyerEmail, int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
